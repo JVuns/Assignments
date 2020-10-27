49c9d1102ec070daa6d3c2e4f3cc1e0eaed0a68b
JVuns <jevon.danaristo@binus.ac.id>
2020-10-27 20:51:49 +0800

    Sumarry

--- 5/Assignment_5.py
+++ 5/Assignment_5.py
@@ -1,3 +1,11 @@
-longtext = open('thebook.txt')
-for words in longtext:
-    print(words)
+import re
+def hapax(filepath):
+    file = open(filepath, encoding="utf8") 
+    words = re.findall('\w+', file.read().lower())
+    count = {key: 0 for key in words} #var store count
+    for word in words:  #iterate if word is already in the sequence datatype
+        count[word] += 1
+    for word in count:  #add new sequence datatype for new word
+        if count[word] == 1:
+            print (word)    
+hapax('thebook.txt')
