720c24cad9a74ccc80e4814cd1b13d5c1c592243
JVuns <jevon.danaristo@binus.ac.id>
2020-10-27 22:28:54 +0800

    Summary

--- 6/Assingment_6.py
+++ 6/Assingment_6.py
@@ -3,9 +3,12 @@ with open("file.txt",mode="r",encoding="utf8") as rfile, open("2ndtext", mode="w
     for lines in rfile:
         longlist.append(lines.split())
     # print(longlist)
+        linecount = 0
     for list1 in longlist:
         # print(list1)
+        linecount += 1
+        acceptablecount = str(linecount)
         str1 = " "   
         dud = (str1.join(list1))
-        wfile.write(dud+"\n")
-        print(dud)
+        wfile.write(str(linecount)+" "+dud+"\n")
+
